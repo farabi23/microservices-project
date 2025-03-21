5cdad453043e370af5b16833834aee42f1c3b487
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-03-22 02:34:12 +0500

    controllers,service, repo,

--- product-service/src/main/java/com/example/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/example/product_service/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.example.product_service.repository;
 
+import com.example.product_service.entity.Category;
 import com.example.product_service.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findProductsByCategoryByCategory(Category category);
 }
--- product-service/src/main/java/com/example/product_service/service/ProductService.java
+++ product-service/src/main/java/com/example/product_service/service/ProductService.java
@@ -1,8 +1,43 @@
 package com.example.product_service.service;
 
+import com.example.product_service.entity.Category;
+import com.example.product_service.entity.Product;
+import com.example.product_service.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+
+    }
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product) {
+        productRepository.save(product);
+    }
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> getProductsByCategory(Category category) {
+        return productRepository.findProductsByCategoryByCategory(category);
+    }
 
 }
