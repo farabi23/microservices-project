8d5d4c6baf0d216af63d18f5c4172945a6270f0c
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-04-04 15:51:18 +0500

    main page on front-end

--- front-end/src/main/java/com/front_end/front_end/MainPage/MainController.java
+++ front-end/src/main/java/com/front_end/front_end/MainPage/MainController.java
@@ -1,12 +1,37 @@
 package com.front_end.front_end.MainPage;
 
 
+import com.front_end.front_end.config.WebClientConfig;
+import com.front_end.front_end.entities.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private final WebClient webClient;
 
+    public MainController(WebClient webClient) {
+        this.webClient = WebClient.builder().baseUrl("http://localhost:8086").build();
+    }
 
+    @GetMapping("/")
+    public String products(Model model) {
 
+        Mono<List<Product>> productListMono = webClient.get()
+                .uri("/products")
+                .retrieve()
+                .bodyToFlux(Product.class)
+                .collectList();
+
+        List<Product> productList = productListMono.block();
+
+        model.addAttribute("products", productList);
+        return "main";
+    }
 }
---
+++ front-end/src/main/java/com/front_end/front_end/entities/Product.java
@@ -0,0 +1,68 @@
+package com.front_end.front_end.entities;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Category category;
+
+    public Product(){
+
+    }
+
+    public Product(long id, String name, String description, BigDecimal price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
